ce48d93669642fb87e878714654fab751b82c544
Vladislav0303 <vladshulyak003@gmail.com>
2025-09-24 11:40:54 +0300

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,14 @@
+public class Client {
+    String nameClient;
+    String password;
+    public Client() {
+    }
+    public void client() {
+//    створюємо об'єкт Site s = new Site();, щоб записати дані користувача в базу даних
+//    я буду записувати BufferedWriter/BufferedRider
+//    s.database = "Логін користувача: " + nameClient + " Пароль: " + password;
+//    після того, як зареєструвалися, авторизуємося тобто робимо рекурсію методу
+//    далі клас клас Product
+        Product product = new Product();
+    }
+}
