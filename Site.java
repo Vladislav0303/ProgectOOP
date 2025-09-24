af4a7297a65bd853a597f9918253d7fb481dc701
Vladislav0303 <vladshulyak003@gmail.com>
2025-09-24 11:41:06 +0300

    Add files via upload

---
+++ Site.java
@@ -0,0 +1,14 @@
+
+public class Site {
+    String database = "data.txt";
+    String[] communications;
+    public void site() {
+        // communications - виводимо зв'язки(Твітер, телеграм, Х)
+        // Далі в клас клієнт авторизація/реєстрація
+        // Або в клас адмін для авторизації як адмін
+    }
+    public static void main(String[] args) {
+        Site s = new Site();
+        s.toString();
+    }
+}
