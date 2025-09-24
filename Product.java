7cfcfae533016b24b8fdf90a02c9d6e2fff5bc38
Vladislav0303 <vladshulyak003@gmail.com>
2025-09-24 11:41:20 +0300

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,14 @@
+
+public class Product {
+    String nameProduct;
+    int countOfProduct;
+    int cost;
+    String characteristic;
+    int rating;
+    public Product() {
+        
+    }
+    public Product(String nameProduct, int countOfProduct, int cost, String characteristic, int rating) {
+        Response rsp = new Response();
+    }
+}
