83c6f8925db62dc98d9702a15a718e0b7e9eb921
Vladislav0303 <vladshulyak003@gmail.com>
2025-09-27 18:08:21 +0300

    Add files via upload

--- Admin.java
+++ Admin.java
@@ -1,2 +1,15 @@
 public class Admin {
+    String NameAdmin;
+    String password;
+
+    public Admin() {
+
+    }
+    public void admin() {
+//     Site site = new Site();
+//     System.out.println(site.database);
+//     привілеї адміна
+//     Product pr = new Product(); - вводить назву товару, характеристику, і т.д
+//     також може взаємодіяти з складом - Sklad sklad = new Sklad();
+    }
 }
