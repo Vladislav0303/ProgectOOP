65ac45678d8e8ea99b3ba73b285917fffeb79c75
Vladislav0303 <vladshulyak003@gmail.com>
2025-10-21 19:57:26 +0300

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,18 +1,41 @@
+import java.util.Scanner;
+
 public class Client {
     public String nameClient;
     private String password;
+    Scanner scanner = new Scanner(System.in);
 
     public Client() {
-        client();
+
     }
     void client() {
-   Site s = new Site();
-   s.setDatabase(nameClient + " " + password);
-//    я буду записувати BufferedWriter/BufferedRider
-//    s.database = "Логін користувача: " + nameClient + " Пароль: " + password;
-//    після того, як зареєструвалися, авторизуємося тобто робимо рекурсію методу
-        s.site();
-//    далі клас Product
-        Product product = new Product();
+        Site s = new Site();
+        Admin admin = new Admin();
+        System.out.println("Введіть ім'я: ");
+        nameClient = scanner.nextLine();
+        System.out.println("Введіть пароль: ");
+        password = scanner.nextLine();
+        s.setDatabase(nameClient + " " + password);
+        if(nameClient.equals(admin.NameAdmin)) {
+            if(password.equals(admin.getPasswordAdmin())) {
+                System.out.println("Ви увійшли як Адмін");
+                admin.admin();
+            } else {
+                System.out.println("Пароль не вірний");
+                client();
+            }
+        } else {
+            System.out.println("Ви увійшли у систему");
+            Product product = new Product();
+            product.product();
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
