790661ed3b13131efc135ef67274cadfd1ccce70
Vladislav0303 <vladshulyak003@gmail.com>
2025-10-21 20:00:09 +0300

    Add files via upload

--- Admin.java
+++ Admin.java
@@ -1,20 +1,36 @@
+import java.util.Scanner;
+
 public class Admin {
-    protected String NameAdmin;
-    private String password;
+    protected String NameAdmin = "Vlad";
+    private String passwordAdmin = "Vlad123";
+    Scanner sc = new Scanner(System.in);
 
     public Admin() {
-        admin();
+
     }
     void admin() {
-     Site site = new Site();
-        site.setDatabase(NameAdmin + " " + password);
-//        site.setCommunications();
+        Site s = new Site();
+        s.setDatabase(NameAdmin + " " + passwordAdmin);
+//        setCommunications();
 //     привілеї адміна
-        String data = site.getDatabase();
-        System.out.println(data);
+        //String data = getDatabase();
+        //System.out.println(data);
+        //
         Product pr = new Product();
-        pr.aboutOfProduct();
-        Delivery del = new Delivery();
+        System.out.println("Введіть назву товару: ");
+        String nameProd = sc.nextLine();
+        System.out.println("Введіть ціну на товар: ");
+        int costProd = sc.nextInt();
+        System.out.println("Введіть характеристику товару: ");
+        Scanner sc1 = new Scanner(System.in);
+        String characteristic = sc1.nextLine();
+        System.out.println("Введіть кількість товару який є в наявності: ");
+        int countOfProduct = sc.nextInt();
+        System.out.println("Введіть оцінку товару: ");
+        int rating = sc.nextInt();
+        pr.aboutOfProduct(nameProd, costProd, characteristic, countOfProduct, rating);
+        //
+        //Delivery del = new Delivery();
 //        це буде зберагітися в обазу даних
 //        del.getTypeofdelivery();
 //        del.getTypeofcountry();
@@ -23,5 +39,13 @@ public class Admin {
 //        sklad.setNumOfSklad();
 //        sklad.setIsavailability();
 //        sklad.setCountOfTovar();
+    }
+
+    public String getPasswordAdmin() {
+        return passwordAdmin;
+    }
+
+    public void setPasswordAdmin(String passwordAdmin) {
+        this.passwordAdmin = passwordAdmin;
     }
 }
